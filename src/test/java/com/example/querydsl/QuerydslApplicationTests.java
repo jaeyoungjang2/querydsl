a6c1bf513ab49b741119d2d97c2a75e78b14a579
lion8548 <lion8548@jiran.com>
2023-06-20 14:11:16 +0900

    feat: querydsl 설정 추가 및 querydsl 동작 테스트

--- src/test/java/com/example/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/example/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,48 @@
 package com.example.querydsl;
 
+import com.example.querydsl.entity.Hello;
+import com.example.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+//    자바 표준 스펙에서는 아래와 같이 사용
+    @PersistenceContext
+//    spring frameWork에서는 아래와 같이 사용할 수 있습니다.
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+//        h는 alias입니다.
+//        query와 관련된 것은 q타입을 이용합니다.
+//        QHello qHello = new QHello("h");
+//        위와 같이 new로 만들 필요는 없습니다.
+        QHello qHello = QHello.hello;
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
