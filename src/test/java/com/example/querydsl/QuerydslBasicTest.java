bd93e76eeb797a1b39a88e56e6ca384881dcdfe3
jaeyoungjang2 <lion85488@gmail.com>
2023-06-22 21:22:34 +0900

    feat: 기본 조인과 세타 조인

--- src/test/java/com/example/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/example/querydsl/QuerydslBasicTest.java
@@ -263,4 +263,49 @@ public class QuerydslBasicTest {
 //        assertThat(teamB.get(team.name)).isEqualTo("teamB");
 //        assertThat(teamB.get(member.age.avg())).isEqualTo(35); // (30 + 40) / 2
     }
+
+    /**
+     * 팀 A에 소속된 모든 회원
+     */
+    @Test
+    public void join() throws Exception {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
+
+    /**
+     * 연관 관계가 없는 세타 조인
+     * 회원의 이름이 팀 이름과 같은 회원을 조회
+     * 모든 member와 모든 team을 다 가지고 와서 member.name과 team.name을 비교해서 join
+     *
+     * 주의점
+     * 외부 조인 불가능 (leftOuterJoin, rightOuterJoin)
+     */
+    @Test
+    public void theta_join() throws Exception {
+        //given
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        // when
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        // then
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+    }
 }
